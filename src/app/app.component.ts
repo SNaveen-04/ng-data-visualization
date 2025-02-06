9aac17af24b9ea774208b3c9b391339588c02c3d
dineshraja <dineshrajaseven@gmail.com>
2025-02-06 15:02:25 +0530

    bar chart added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgxChartsModule} from '@swimlane/ngx-charts'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'visualization-project';
+ 
 }
