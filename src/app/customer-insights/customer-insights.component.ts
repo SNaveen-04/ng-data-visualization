206388dfcc52ecd997f681ad5267547bdac1597e
pooja <poojaelangovan516@gmail.com>
2025-02-06 15:09:27 +0530

    index file change

---
+++ src/app/customer-insights/customer-insights.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-insights',
+  imports: [],
+  templateUrl: './customer-insights.component.html',
+  styleUrl: './customer-insights.component.css'
+})
+export class CustomerInsightsComponent {
+
+}
