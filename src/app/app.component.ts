e513d300d9b69966da97603c2acbd5faa17190ea
dineshraja <dineshrajaseven@gmail.com>
2025-02-06 15:15:23 +0530

    bar chart added to the shared folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
+import { DropDownComponent } from "./shared/drop-down/drop-down.component";
 import {NgxChartsModule} from '@swimlane/ngx-charts'; 
+import { BarChartComponent } from './shared/bar-chart/bar-chart/bar-chart.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [NavbarComponent, RouterOutlet, DropDownComponent,BarChartComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'visualization-project';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { DropDownComponent } from './shared/drop-down/drop-down.component';
+
 export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'analysis',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    component: DropDownComponent,
+  },
 ];
