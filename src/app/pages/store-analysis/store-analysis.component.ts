6420facd0bbb228fbc2e50b980e9fedea772a1a5
dineshraja <dineshrajaseven@gmail.com>
2025-02-07 15:19:07 +0530

    bar chart is set up for the store-analysis-component

--- src/app/pages/store-analysis/store-analysis.component.ts
+++ src/app/pages/store-analysis/store-analysis.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HorizontalBarChartComponent } from '../../shared/d3-bar-chart/horizontal-bar-chart/horizontal-bar-chart.component';
 
 @Component({
   selector: 'app-store-analysis',
-  imports: [],
+  imports: [HorizontalBarChartComponent],
   templateUrl: './store-analysis.component.html',
   styleUrl: './store-analysis.component.css'
 })
