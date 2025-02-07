d38edf33c410c7fb6c839b2ed532fd8b466194de
SNaveen-04 <naveen80567@gmail.com>
2025-02-07 15:29:57 +0530

    merge bar chart

--- src/app/pages/store-analysis/store-analysis.component.ts
+++ src/app/pages/store-analysis/store-analysis.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { LineChartComponent } from "../../shared/line-chart/line-chart.component";
 
 @Component({
   selector: 'app-store-analysis',
-  imports: [],
+  imports: [LineChartComponent],
   templateUrl: './store-analysis.component.html',
   styleUrl: './store-analysis.component.css'
 })
