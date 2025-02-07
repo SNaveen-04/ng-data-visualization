49f287ca2a4180dcdc8b2972d93323d3b6fc2016
SNaveen-04 <naveen80567@gmail.com>
2025-02-07 12:42:26 +0530

    5 graph page template completed

---
+++ src/app/pages/department-analysis/department-analysis.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-department-analysis',
+  imports: [],
+  templateUrl: './department-analysis.component.html',
+  styleUrl: './department-analysis.component.css'
+})
+export class DepartmentAnalysisComponent {
+
+}
---
+++ src/app/pages/product-analysis/product-analysis.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-analysis',
+  imports: [],
+  templateUrl: './product-analysis.component.html',
+  styleUrl: './product-analysis.component.css'
+})
+export class ProductAnalysisComponent {
+
+}
