a4a8f84716e76a963d4f6f4f09c254127f729dfb
SNaveen-04 <naveen80567@gmail.com>
2025-02-07 15:38:34 +0530

    bug

--- src/app/pages/store-analysis/store-analysis.component.ts
+++ src/app/pages/store-analysis/store-analysis.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { LineChartComponent } from '../../shared/line-chart/line-chart.component';
-import { HorizontalBarChartComponent } from '../../shared/d3-bar-chart/horizontal-bar-chart/horizontal-bar-chart.component';
 
 @Component({
   selector: 'app-store-analysis',
-  imports: [HorizontalBarChartComponent, LineChartComponent],
+  imports: [LineChartComponent],
   templateUrl: './store-analysis.component.html',
   styleUrl: './store-analysis.component.css',
 })
