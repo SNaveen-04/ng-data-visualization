fbe72c14a798553595685547c4eafc78d3908d48
dineshraja <dineshrajaseven@gmail.com>
2025-02-07 10:53:06 +0530

    pulling the remote main to local main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
-import { DropDownComponent } from "./shared/drop-down/drop-down.component";
-
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, RouterOutlet, DropDownComponent],
+  imports: [NavbarComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
