323fa94a57f28b8aad6d2259068d984a10780271
dineshraja <dineshrajaseven@gmail.com>
2025-02-07 11:32:43 +0530

    sorting the bar chart value based on the title property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
+import { HorizontalBarChartComponent } from "./shared/d3-bar-chart/horizontal-bar-chart/horizontal-bar-chart.component";
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, RouterOutlet],
+  imports: [NavbarComponent, RouterOutlet, HorizontalBarChartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  barChartName:string="Top selling products"
- 
+  barChartName:string="Top selling products";
+   title="App component";
 }
