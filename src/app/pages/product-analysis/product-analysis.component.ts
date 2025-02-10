485dad3ac93acce30b9b371c28fd9afaafcde6bb
pooja <poojaelangovan516@gmail.com>
2025-02-10 14:40:40 +0530

    Cross selling

--- src/app/pages/product-analysis/product-analysis.component.ts
+++ src/app/pages/product-analysis/product-analysis.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
 import { CustomerInsightsComponent } from '../../shared/customer-insights/customer-insights.component';
-import { LineChartComponent } from '../../shared/line-chart/line-chart.component';
 import { CrossSellingProductsComponent } from '../../shared/cross-selling-products/cross-selling-products.component';
 
 @Component({
   selector: 'app-product-analysis',
-  imports: [
-    CustomerInsightsComponent,
-    LineChartComponent,
-    CrossSellingProductsComponent,
-  ],
+  imports: [CustomerInsightsComponent, CrossSellingProductsComponent],
   templateUrl: './product-analysis.component.html',
   styleUrl: './product-analysis.component.css',
 })
