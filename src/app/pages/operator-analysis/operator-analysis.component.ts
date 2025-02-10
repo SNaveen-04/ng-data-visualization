180cd67e8fc70aa3a8a5afa22b9728dfed5548db
pooja <poojaelangovan516@gmail.com>
2025-02-10 14:17:18 +0530

    Cross selling

--- src/app/pages/operator-analysis/operator-analysis.component.ts
+++ src/app/pages/operator-analysis/operator-analysis.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CustomerInsightsComponent } from '../../shared/customer-insights/customer-insights.component';
+import { LineChartComponent } from '../../shared/line-chart/line-chart.component';
+import { ProductSalesComponent } from '../../shared/product-sales/product-sales.component';
 
 @Component({
   selector: 'app-operator-analysis',
-  imports: [],
+  imports: [
+    CustomerInsightsComponent,
+    LineChartComponent,
+    ProductSalesComponent,
+  ],
   templateUrl: './operator-analysis.component.html',
-  styleUrl: './operator-analysis.component.css'
+  styleUrl: './operator-analysis.component.css',
 })
-export class OperatorAnalysisComponent {
-
-}
+export class OperatorAnalysisComponent {}
--- src/app/pages/product-analysis/product-analysis.component.ts
+++ src/app/pages/product-analysis/product-analysis.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CustomerInsightsComponent } from '../../shared/customer-insights/customer-insights.component';
+import { LineChartComponent } from '../../shared/line-chart/line-chart.component';
+import { CrossSellingProductsComponent } from '../../shared/cross-selling-products/cross-selling-products.component';
 
 @Component({
   selector: 'app-product-analysis',
-  imports: [],
+  imports: [
+    CustomerInsightsComponent,
+    LineChartComponent,
+    CrossSellingProductsComponent,
+  ],
   templateUrl: './product-analysis.component.html',
-  styleUrl: './product-analysis.component.css'
+  styleUrl: './product-analysis.component.css',
 })
-export class ProductAnalysisComponent {
-
-}
+export class ProductAnalysisComponent {}
