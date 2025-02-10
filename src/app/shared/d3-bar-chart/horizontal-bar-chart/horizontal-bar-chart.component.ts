6f9d193a9ede0fb78e329e0774ea8feb77021ebb
SNaveen-04 <naveen80567@gmail.com>
2025-02-10 15:42:11 +0530

    ui alignment merge fix

--- src/app/shared/d3-bar-chart/horizontal-bar-chart/horizontal-bar-chart.component.ts
+++ src/app/shared/d3-bar-chart/horizontal-bar-chart/horizontal-bar-chart.component.ts
@@ -8,11 +8,16 @@ interface ChartData {
 @Component({
   selector: 'app-horizontal-bar-chart',
   templateUrl: './horizontal-bar-chart.component.html',
-  standalone:true,
-  styleUrls: ['./horizontal-bar-chart.component.css']
+  standalone: true,
+  styleUrls: ['./horizontal-bar-chart.component.css'],
 })
 export class HorizontalBarChartComponent implements OnInit {
-
+  data1: ChartData[] = [
+    {
+      name: 'Germany',
+      value: 50000,
+    },
+    {
       name: 'United States',
       value: 30000,
     },
@@ -37,30 +42,6 @@ export class HorizontalBarChartComponent implements OnInit {
   @Input() browser_height = 250;
   // brower_width=500    //this is width of svg for the desktop screen
   barWidth = 0.8; // adjust this to handle the width of the bar
-      "name": "United States",
-      "value": 30000,
-    },
-    {
-      "name": "France",
-      "value": 40000,
-    },
-    {
-      "name": "Cheese Bread",
-      "value": 10000,
-    },
-    {
-      "name": "Spain",
-      "value": 20000,
-    }
-  ];
-  
-  @Input() title="Top selling products";
-  @Input() color='#50C878';
-  @Input() data:any[]=this.data1;
-  @Input() brower_width = 300;
-  @Input() browser_height = 250;
-  // brower_width=500    //this is width of svg for the desktop screen
-   barWidth=0.8;   // adjust this to handle the width of the bar
   @ViewChild('chart', { static: true }) private chartContainer!: ElementRef;
   constructor() {}
   ngOnInit(): void {
@@ -68,7 +49,7 @@ export class HorizontalBarChartComponent implements OnInit {
   }
 
   private createChart(): void {
-    if(this.title==='Top selling products') {
+    if (this.title === 'Top selling products') {
       this.data.sort((a, b) => b.value - a.value);
     } else {
       this.data.sort((a, b) => a.value - b.value);
@@ -79,21 +60,24 @@ export class HorizontalBarChartComponent implements OnInit {
     const margin = { top: 10, right: 10, bottom: 40, left: 100 }; // Increased left margin to provide space for labels
     const width = this.brower_width - margin.left - margin.right;
     const height = this.browser_height - margin.top - margin.bottom;
-    const svg = d3.select(element)
+    const svg = d3
+      .select(element)
       .append('svg')
       .attr('width', width + margin.left + margin.right)
       .attr('height', height + margin.top + margin.bottom)
       .append('g')
       .attr('transform', `translate(${margin.left},${margin.top})`);
-  
+
     const tooltip = d3.select('#tooltip');
 
-    const x = d3.scaleLinear()
-      .domain([0, d3.max(data, d => d.value) ?? 0])
-      .range([0, width-10]);
-  
-    const y = d3.scaleBand()
-      .domain(data.map(d => d.name))
+    const x = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, (d) => d.value) ?? 0])
+      .range([0, width - 10]);
+
+    const y = d3
+      .scaleBand()
+      .domain(data.map((d) => d.name))
       .range([0, height])
       .padding(0.2);
 
@@ -111,12 +95,11 @@ export class HorizontalBarChartComponent implements OnInit {
 
     svg
       .selectAll('.bar')
-
       .data(data)
       .enter()
       .append('rect')
       .attr('class', 'bar')
-      .attr('y', d => (y(d.name) ?? 0) + 5)
+      .attr('y', (d) => (y(d.name) ?? 0) + 5)
       .attr('width', 0) // Start with width 0 for animation
       .attr('height', y.bandwidth() - 20)
       .attr('fill', this.color)
@@ -136,15 +119,13 @@ export class HorizontalBarChartComponent implements OnInit {
       .duration(800) // Duration of the animation in milliseconds
       .attr('width', (d) => (x(d.value) ?? 0) * this.barWidth); // Reduce width by 20%
 
-
     const labels = svg.selectAll('.bar-label').data(data).enter();
-
 
     labels
       .append('text')
       .attr('class', 'value-label')
-      .attr('y', d => (y(d.name) ?? 0) + (y.bandwidth() / 2 - 4))
-      .attr('x', d => (x(d.value) ?? 0) * this.barWidth + 10) // Adjust label position accordingly
+      .attr('y', (d) => (y(d.name) ?? 0) + (y.bandwidth() / 2 - 4))
+      .attr('x', (d) => (x(d.value) ?? 0) * this.barWidth + 10) // Adjust label position accordingly
       .attr('font-weight', 'lighter')
       .attr('fill', '#2222222')
       .text((d) => d.value)
